58d391e40d7718b92d5511b191e7732c161437ad
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-04-28 12:28:52 +0200

    Initial commit

---
+++ source/models/myname/package.d
@@ -0,0 +1,9 @@
+module models.myname;
+
+public import models.myname.attributes;
+public import models.myname.classes;
+public import models.myname.elements;
+public import models.myname.entities;
+public import models.myname.interfaces;
+public import models.myname.usecases;
+public import models.myname.values;
