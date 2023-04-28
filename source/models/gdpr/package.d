303bc504a98bd16054956190353a3342688c80b4
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-04-28 12:31:11 +0200

    Update name

---
+++ source/models/gdpr/package.d
@@ -0,0 +1,9 @@
+module models.gdpr;
+
+public import models.gdpr.attributes;
+public import models.gdpr.classes;
+public import models.gdpr.elements;
+public import models.gdpr.entities;
+public import models.gdpr.interfaces;
+public import models.gdpr.usecases;
+public import models.gdpr.values;
