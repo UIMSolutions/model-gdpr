bd873a9a1759d042be2893645761ba30a8b8dd88
Sicherheitsschmiede <ui.manufaktur@gmail.com>
2023-05-01 07:37:09 +0200

    Update package.d

--- source/models/gdpr/package.d
+++ source/models/gdpr/package.d
@@ -4,6 +4,7 @@ public import models.gdpr.attributes;
 public import models.gdpr.classes;
 public import models.gdpr.elements;
 public import models.gdpr.entities;
-public import models.gdpr.interfaces;
 public import models.gdpr.usecases;
 public import models.gdpr.values;
+
+public import models.gdpr.tests;
